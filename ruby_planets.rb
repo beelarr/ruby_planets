6c4ba91b2e2eca2b9d1feda7866cddf7a83a4f03
beelarr <bryonl@me.com>
2017-10-04 18:51:46 -0500

    Finished exercise

--- ruby_planets.rb
+++ ruby_planets.rb
@@ -14,3 +14,21 @@ planet_list.pop
 
 p rocky_planets
 p planet_list
+
+ship_travel = [
+    ["Millennium Falcon", "Mercury"],
+    ["Star Destroyer", "Mars", "Saturn"],
+    ["TIE Fighter", "Venus"],
+    ["SLAVE I", "Earth"]
+]
+
+
+planet_list.each do |planet|
+  ship_travel.each do |ship|
+    if ship.include?(planet)
+      p "#{planet} has been visited by #{ship[0]}"
+    end
+  end
+end
+
+
